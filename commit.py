a802a65194c3bfa76754a80b3fc53e959329ec3f
edersonbrilhante <contato@edersonbrilhante.com.br>
2021-04-07 15:02:54 +0200

    fix: job

--- commit.py
+++ commit.py
@@ -15,10 +15,22 @@ hostname, image = reference.Reference.split_docker_domain(reference.Reference.pa
 tag = reference.Reference.parse(IMAGE).get('tag') or 'latest'
 file_path = '{}/{}/{}.json'.format(hostname, image, tag)
 
-project.files.create({'file_path': file_path,
-    'branch': 'main',
-    'content': base64.b64encode(open(FILE, "rb").read()).decode("utf-8"),
-    'author_email': 'contato@edersonbrilhante.com.br',
-    'author_name': 'Ederson Brilhante',
-    'encoding': 'base64',
-    'commit_message': 'Create {}'.format(file_path)})
+
+content = base64.b64encode(open(FILE, "rb").read()).decode("utf-8")
+commit_message = 'Create {}'.format(file_path)
+branch = 'main'
+encoding = 'base64'
+try:
+    f = project.files.get(file_path=file_path, ref="main")
+except gitlab.exceptions.GitlabGetError as e:
+    if e.response_code == 404:
+        project.files.create({'file_path': file_path,
+            'branch': branch,
+            'content': content,
+            'author_email': 'contato@edersonbrilhante.com.br',
+            'author_name': 'Ederson Brilhante',
+            'encoding': encoding,
+            'commit_message': commit_message})
+else:
+    f.content = content 
+    f.save(branch=branch, commit_message=commit_message, encoding=encoding)
